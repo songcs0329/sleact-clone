e98b34a9b7b8ec4bedef55827b80aa66695d2253
songcs0329 <54650110+songcs0329@users.noreply.github.com>
2022-05-01 16:48:22 +0900

    feat: init router set

--- front/layouts/App.tsx
+++ front/layouts/App.tsx
@@ -1,7 +1,16 @@
 import React from "react"
+import { Redirect, Route, Switch } from "react-router"
+import LogIn from "@pages/LogIn"
+import SignUp from "@pages/SignUp"
 
 const App = () => {
-  return <div>초기 세팅입니다.</div>
+  return (
+    <Switch>
+      <Redirect exact path={"/"} to={"/login"} />
+      <Route path={"/login"} component={LogIn} />
+      <Route path={"/signup"} component={SignUp} />
+    </Switch>
+  )
 }
 
 export default App
