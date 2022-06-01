4c3c304fcea065150bf092b1aad922e7b117a524
songcs0329 <54650110+songcs0329@users.noreply.github.com>
2022-06-01 21:52:41 +0900

    workspace 작업시작

---
+++ front/layouts/Workspace/index.tsx
@@ -0,0 +1,81 @@
+import React, { FC, useCallback, useState } from "react"
+import axios from "axios"
+import useSWR from "swr"
+import fetcher from "@utils/fetcher"
+import { Redirect, Route, Switch } from "react-router-dom"
+import gravatar from "gravatar"
+import loadable from "@loadable/component"
+import {
+  Channels,
+  Chats,
+  Header,
+  LogOutButton,
+  MenuScroll,
+  ProfileImg,
+  ProfileModal,
+  RightMenu,
+  WorkspaceName,
+  Workspaces,
+  WorkspaceWrapper,
+} from "./styles"
+import Menu from "@components/Menu"
+const Channel = loadable(() => import("@pages/Channel"))
+const DirectMessage = loadable(() => import("@pages/DirectMessage"))
+
+const Workspace: FC = ({ children }) => {
+  const { data, error, mutate } = useSWR("http://localhost:3095/api/users", fetcher)
+  const [showUserMenu, setShowUserMenu] = useState(false)
+
+  const onLogout = useCallback(() => {
+    axios
+      .post("http://localhost:3095/api/users/logout", null, {
+        withCredentials: true,
+      })
+      .then(() => {
+        mutate(false, false)
+      })
+  }, [])
+  const onClickUserProfile = useCallback(() => {
+    setShowUserMenu((prev) => !prev)
+  }, [])
+
+  if (!data) return <Redirect to={"/login"} />
+  return (
+    <div>
+      <Header>
+        <RightMenu>
+          <span onClick={onClickUserProfile}>
+            <ProfileImg src={gravatar.url(data.email, { size: "28px", d: "retro" })} alt={data.nickname} />
+            {showUserMenu && (
+              <Menu style={{ right: 0, top: 38 }} show={showUserMenu} onCloseModal={onClickUserProfile}>
+                <ProfileModal>
+                  <img src={gravatar.url(data.email, { size: "36px", d: "retro" })} alt={data.nickname} />
+                  <div>
+                    <span id="profile-name">{data.nickname}</span>
+                    <span id="profile-active">Active</span>
+                  </div>
+                </ProfileModal>
+                <LogOutButton onClick={onLogout}>로그아웃</LogOutButton>
+              </Menu>
+            )}
+          </span>
+        </RightMenu>
+      </Header>
+      <WorkspaceWrapper>
+        <Workspaces>workspaces</Workspaces>
+        <Channels>
+          <WorkspaceName>Sleact</WorkspaceName>
+          <MenuScroll>Menu scroll</MenuScroll>
+        </Channels>
+        <Chats>
+          <Switch>
+            <Route path={"/workspace/channel"} component={Channel} />
+            <Route path={"/workspace/dm"} component={DirectMessage} />
+          </Switch>
+        </Chats>
+      </WorkspaceWrapper>
+    </div>
+  )
+}
+
+export default Workspace
