9eeb27f7ed72cd1d77d571f3de7583a21f4ce0b3
songcs0329 <54650110+songcs0329@users.noreply.github.com>
2022-06-19 17:27:50 +0900

    DM 페이지 작업중

--- front/pages/DirectMessage/index.tsx
+++ front/pages/DirectMessage/index.tsx
@@ -1,7 +1,29 @@
 import React from "react"
+import gravatar from "gravatar"
+import { Container, Header } from "./styles"
+import useSWR from "swr"
+import { IUser } from "@typings/db"
+import fetcher from "@utils/fetcher"
+import { useParams } from "react-router"
+import ChatList from "@components/ChatList"
+import ChatBox from "@components/ChatBox"
 
 const DirectMessage = () => {
-  return <div>DirectMessage</div>
+  const { workspace, id } = useParams<{ workspace?: string; id?: string }>()
+  const { data: myData } = useSWR<IUser>("/api/users", fetcher)
+  const { data: userData } = useSWR<IUser>(`/api/workspaces/${workspace}/users/${id}`, fetcher)
+
+  if (!myData || !userData) return null
+  return (
+    <Container>
+      <Header>
+        <img src={gravatar.url(userData.email, { size: "28px", d: "retro" })} alt={userData?.nickname} />
+        <span>{userData?.nickname}</span>
+      </Header>
+      <ChatList />
+      <ChatBox chat="" />
+    </Container>
+  )
 }
 
 export default DirectMessage
